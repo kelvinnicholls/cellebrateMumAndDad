8333b555cbbe59f5df8371092a2d1f34fa678a13
kelvin <kelvin.nicholls@gmail.com>
2017-10-12 17:02:53 +0100

    karma running but test failing

--- src/app/test.ts
+++ src/app/test.ts
@@ -1,3 +1,36 @@
+// // This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
+// import 'zone.js/dist/long-stack-trace-zone';
+// import 'zone.js/dist/proxy.js';
+// import 'zone.js/dist/sync-test';
+// import 'zone.js/dist/jasmine-patch';
+// import 'zone.js/dist/async-test';
+// import 'zone.js/dist/fake-async-test';
+// import { getTestBed } from '@angular/core/testing';
+// import {
+//   BrowserDynamicTestingModule,
+//   platformBrowserDynamicTesting
+// } from '@angular/platform-browser-dynamic/testing';
+
+// // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
+// declare const __karma__: any;
+// declare const require: any;
+
+// // Prevent Karma from running prematurely.
+// __karma__.loaded = function () {};
+
+// // First, initialize the Angular testing environment.
+// getTestBed().initTestEnvironment(
+//   BrowserDynamicTestingModule,
+//   platformBrowserDynamicTesting()
+// );
+// // Then we find all the tests.
+// const context = require.context('./', true, /\.spec\.ts$/);
+// // And load the modules.
+// context.keys().map(context);
+// // Finally, start Karma to run the tests.
+// __karma__.start();
+
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/dist/long-stack-trace-zone';
@@ -30,3 +63,4 @@ const context = require.context('./', true, /\.spec\.ts$/);
 context.keys().map(context);
 // Finally, start Karma to run the tests.
 __karma__.start();
+
