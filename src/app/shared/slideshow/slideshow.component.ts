5525f5decd267bb368149d87efc4893fd6a1a430
kelvin <kelvin.nicholls@gmail.com>
2017-11-08 14:05:02 +0000

    Sort out slideshow

--- src/app/shared/slideshow/slideshow.component.ts
+++ src/app/shared/slideshow/slideshow.component.ts
@@ -4,6 +4,8 @@ import { SlideShow } from "./slideshow.model";
 import { SlideShowService } from "./slideshow.service";
 import { Consts } from "../../shared/consts";
 
+import { NgxGalleryOptions, NgxGalleryImage } from 'ngx-gallery';
+
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
@@ -12,6 +14,8 @@ import { Consts } from "../../shared/consts";
 export class SlideShowComponent implements OnDestroy {
   slideShow: SlideShow;
   display = 'none';
+
+  title = "TITLE";
 
   private showSlideShowSub: EventEmitter<SlideShow>;
 
@@ -34,12 +38,40 @@ export class SlideShowComponent implements OnDestroy {
   }
 
   ngOnInit() {
+    let galleryImages: NgxGalleryImage[]  = [
+      {
+        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/1-small.jpeg',
+        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/1-medium.jpeg',
+        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/1-big.jpeg'
+      },
+      {
+        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/2-small.jpeg',
+        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/2-medium.jpeg',
+        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/2-big.jpeg'
+      },
+      {
+        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/3-small.jpeg',
+        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/3-medium.jpeg',
+        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/3-big.jpeg'
+      },
+      {
+        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/4-small.jpeg',
+        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/4-medium.jpeg',
+        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/4-big.jpeg'
+      },
+      {
+        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/5-small.jpeg',
+        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/5-medium.jpeg',
+        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/5-big.jpeg'
+      }      
+    ];
 
-
+    this.slideShow = new SlideShow("TiTlE", galleryImages);
     this.showSlideShowSub = this.slideShowService.showSlideShowSub
       .subscribe(
       (slideShow: SlideShow) => {
         this.slideShow = slideShow;
+        this.title = this.slideShow.title;
         this.display = 'block';
       });
   }
