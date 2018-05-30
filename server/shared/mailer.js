c58f20f22e65e839fb0c06abe01e87328a4c0175
kelvin <kelvin.nicholls@gmail.com>
2018-05-30 14:29:24 +0100

    change mailer to reject unauthorized

--- server/shared/mailer.js
+++ server/shared/mailer.js
@@ -9,14 +9,22 @@ const {
   CONSTS
 } = require('../shared/consts');
 
+// const transporter = nodemailer.createTransport({
+//   service: process.env.NODEMAILER_SERVICE,
+//   auth: {
+//     user: process.env.NODEMAILER_EMAIL,
+//     pass: process.env.NODEMAILER_PASSWORD
+//   },
+//   tls: {
+//       rejectUnauthorized: false
+//   }
+// });
+
 const transporter = nodemailer.createTransport({
   service: process.env.NODEMAILER_SERVICE,
   auth: {
     user: process.env.NODEMAILER_EMAIL,
     pass: process.env.NODEMAILER_PASSWORD
-  },
-  tls: {
-      rejectUnauthorized: false
   }
 });
 
